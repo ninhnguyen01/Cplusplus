6c49db82c0a31ff53be8bcdb5786d1726d3151cc
Ninh Nguyen (阮安宁) <ninhnguyenweb@gmail.com>
2022-08-09 21:43:10 -0700

    Add files via upload

---
+++ ExerciseComplete/getStarted.cpp
@@ -0,0 +1,10 @@
+// Learning C++ 
+// Exercise 01_03
+// Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main() {
+    std::cout <<"Hi There!"<< std::endl;
+    return (0);
+}
---
+++ ExerciseComplete/getStarted3.cpp
@@ -0,0 +1,16 @@
+// Learning C++ 
+// Challenge 01_04
+// Console Interaction, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string str;
+    cout << "Enter your name: ";
+	cin >> str;
+	cout << "Nice to meet you, " << str << "!" << endl;
+	return(0);
+}
